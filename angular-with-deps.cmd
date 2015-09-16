30d8373e0f1c95878d31bca56fb62f9ffe9b79fb
BartGo <BartGo@users.noreply.github.com>
2015-09-16 12:47:00 +0200

    Update angular-with-deps.cmd

--- angular-with-deps.cmd
+++ angular-with-deps.cmd
@@ -1,8 +1,6 @@
-rem *** run with admin permissions (elevated)
-rem *** todo: set this also for git bash prompt? or not?
-rem *** C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat"
+rem *** open cmd.exe of nodejs, with admin permissions (elevated)
 
-rem *** proxy
+rem *** proxy for npm
 npm config set proxy http://"user:pass"@squid.mycompany.country:3128
 
 rem *** "-g": Install package globally. Global packages are usually for executable commands.
@@ -11,10 +9,11 @@ npm install -g bower
 
 x:
 git clone "https://github.com/angular/angular.js.git"
+
+rem *** https://github.com/angular/angular-phonecat/issues/141
+git config --global url."https://".insteadOf git:// 
 cd angular.js
 npm install
-
-rem *** CRASH
 
 bower install
 grunt package
