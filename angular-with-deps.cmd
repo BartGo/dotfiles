9eb22cc951354460a05c44369112ad06d2d500a3
BartGo <bartoszgo@gmail.com>
2017-06-05 13:43:47 +0200

    ...

--- angular-with-deps.cmd
+++ angular-with-deps.cmd
@@ -1,11 +1,24 @@
 rem *** open cmd.exe of nodejs, with admin permissions (elevated)
 
+set HTTP_PROXY=
+set HTTPS_PROXY=
+
 rem *** proxy for npm
-npm config set proxy http://"user:pass"@squid.mycompany.country:3128
+rem #npm config edit
+rem #npm config rm proxy
+rem #npm config rm https-proxy
+rem #npm config set proxy "$HTTP_PROXY"
+rem #npm config set https-proxy "$HTTPS_PROXY"
+
+call npm cache clean
+call npm rm proxy
+call npm config set registry "http://registry.npmjs.org/"
+call npm config set strict-ssl false
+call npm config list | find "proxy"
 
 rem *** "-g": Install package globally. Global packages are usually for executable commands.
-npm install -g grunt-cli
-npm install -g bower
+call npm install -g grunt-cli
+call npm install -g bower
 
 x:
 git clone "https://github.com/angular/angular.js.git"
@@ -13,7 +26,7 @@ git clone "https://github.com/angular/angular.js.git"
 rem *** https://github.com/angular/angular-phonecat/issues/141
 git config --global url."https://".insteadOf git:// 
 cd angular.js
-npm install
+call npm install
 
 bower install
 grunt package
