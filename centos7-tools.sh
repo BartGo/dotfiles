72f6a971af8173ccc652e06cdbe1303fb0e1f080
BartGo <BartGo@users.noreply.github.com>
2020-01-29 22:43:00 +0100

    Create centos7-tools.sh

---
+++ centos7-tools.sh
@@ -0,0 +1,5 @@
+
+yum install mc git tig python3 sqlite nano lynx htop postgresql postgresql-server postgresql-contrib \
+  epel-release nginx mod_wsgi tmux screen fortune-mod banner cowsay
+
+banner hello
