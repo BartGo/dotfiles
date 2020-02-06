68c092672a2a39d4ddf5957cc9b33fe8b50e11d4
BartGo <BartGo@users.noreply.github.com>
2020-02-06 19:38:16 +0100

    Create debian-tools.sh

---
+++ debian-tools.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get install install mc git tig python3 python3-pip sqlite nano lynx htop tmux screen fortune-mod cowsay
+
+#postgresql postgresql-server postgresql-contrib epel-release nginx mod_wsgi 
