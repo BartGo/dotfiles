4e98f9671500031338902c7ea8f1d06049a96447
BartGo <BartGo@users.noreply.github.com>
2020-02-06 23:06:24 +0100

    Update debian-tools.sh

--- debian-tools.sh
+++ debian-tools.sh
@@ -1,4 +1,13 @@
+mkdir /x
+cd /x
+git clone https://github.com/BartGo/dotfiles
+git clone https://github.com/BartGo/python-cuturl
 apt-get update
-apt-get install install mc git tig python3 python3-pip sqlite nano lynx htop tmux screen fortune-mod cowsay
+apt-get install mc git tig python3 python3-pip sqlite nano lynx htop tmux screen fortune-mod cowsay
+
+apt-get install locale locales-all
+locale
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen && update-locale LANG=en_US.UTF-8
 
 #postgresql postgresql-server postgresql-contrib epel-release nginx mod_wsgi 
